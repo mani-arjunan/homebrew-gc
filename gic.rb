cdc375c2dcdaa01716bd94278b493c2d23e41183
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-07-30 00:35:25 +0530

    Update gic.rb

--- gic.rb
+++ gic.rb
@@ -1,7 +1,7 @@
 class Gic < Formula
   desc "Github clone CLI tool"
   homepage "https://github.com/mani-arjunan/git-clone-cli"
-  url "https://github.com/mani-arjunan/git-clone-cli/releases/download/v2/git-clone-cli"
+  url "https://github.com/mani-arjunan/git-clone-cli/releases/download/v3/gic"
   sha256 "d1b9fd1e43a92092c49055df7381a6258f8e15b320780077771ebb44e09a8b8e"
   version "0.3.0"
 
