895b8d1a25b930ee6a55e5c80b483938ac4570fc
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-07-30 00:17:08 +0530

    Update git-clone-cli.rb

--- git-clone-cli.rb
+++ git-clone-cli.rb
@@ -2,7 +2,7 @@ class GitCloneCli < Formula
   desc "Github clone CLI tool"
   homepage "https://github.com/mani-arjunan/git-clone-cli"
   url "https://github.com/mani-arjunan/git-clone-cli/releases/download/v1/git-clone-cli"
-  sha256 "4366cbc79b3a26b4ba894838c103d4e5e6e1b5a593daf4c49703bd57120dbefe  git-clone-cli"
+  sha256 "4366cbc79b3a26b4ba894838c103d4e5e6e1b5a593daf4c49703bd57120dbefe"
   version "0.1.0"
 
   def install
