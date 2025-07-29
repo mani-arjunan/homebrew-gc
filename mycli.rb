cb21b27a7e3d2b5186d41235bd3d996dd250a103
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-07-30 00:04:22 +0530

    Create mycli.rb

---
+++ mycli.rb
@@ -0,0 +1,11 @@
+class Mycli < Formula
+  desc "Github clone CLI tool"
+  homepage "https://github.com/mani-arjunan/git-clone-cli"
+  url "https://github.com/mani-arjunan/git-clone-cli/releases/download/v1/git-clone-cli"
+  sha256 "4366cbc79b3a26b4ba894838c103d4e5e6e1b5a593daf4c49703bd57120dbefe  git-clone-cli"
+  version "0.1.0"
+
+  def install
+    bin.install "git-clone-cli-darwin-amd64" => "git-clone-cli"
+  end
+end
