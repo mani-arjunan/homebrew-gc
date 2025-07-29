bdf2048de308f656ac75d3ecbda5e6fee8fd217f
Manikandan Arjunan <31544468+mani-arjunan@users.noreply.github.com>
2025-07-30 00:32:49 +0530

    Update and rename gc.rb to gic.rb

--- gic.rb
+++ gic.rb
@@ -1,11 +1,11 @@
-class Gc < Formula
+class Gic < Formula
   desc "Github clone CLI tool"
   homepage "https://github.com/mani-arjunan/git-clone-cli"
   url "https://github.com/mani-arjunan/git-clone-cli/releases/download/v2/git-clone-cli"
   sha256 "d1b9fd1e43a92092c49055df7381a6258f8e15b320780077771ebb44e09a8b8e"
-  version "0.1.0"
+  version "0.3.0"
 
   def install
-    bin.install "gc" => "gc"
+    bin.install "gic" => "gic"
   end
 end
